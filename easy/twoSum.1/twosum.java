f58f8ea1c38351aa1ef967e1eb2d736a837da68d
scottuzamaki <scottuzamaki@gmail.com>
2022-10-26 13:36:45 -0700

    updated

--- easy/twoSum.1/twosum.java
+++ easy/twoSum.1/twosum.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+
         int[] answer = new int[2];
 
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -7,15 +8,15 @@ public class Solution {
         for (int i = 0; i < nums.length; i ++) {
 
             if (map.containsKey(target - nums[i])) {
-                result[0] = map.get(target - nums[i]);
-                result[1] = i;
-                return result;
+                answer[0] = map.get(target - nums[i]);
+                answer[1] = i;
+                return answer;
             }
 
             map.put(nums[i], i);
         }
 
         
-        return result;
+        return answer;
     }
 }
